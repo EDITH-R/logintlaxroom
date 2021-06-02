64132546c5d36db1355dc1099646f5e6287cdf84
EDITH-R <rese-97@hotmail.com>
2021-06-02 14:35:18 -0500

    cambiando informacion de conexion

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
     
     try{
-         $conexion = new PDO('mysql:host=localhost;dbname=tlax', 'root', '');
+         $conexion = new PDO('mysql:host=klbcedmmqp7w17ik.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306;dbname=m44g2f3mhvnwmm54', 'w61xlg76nntfapxd', 'mmy0ca819v07kxej');
     }catch(PDOException $prueba_error){
         echo "Error: " . $prueba_error->getMessage();
     }
--- login.php
+++ login.php
@@ -13,7 +13,7 @@
         $clave = hash('sha512', $clave);
         
         try{
-            $conexion = new PDO('mysql:host=localhost;dbname=tlax', 'root', '');
+            $conexion = new PDO('mysql:host=klbcedmmqp7w17ik.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306;dbname=m44g2f3mhvnwmm54', 'w61xlg76nntfapxd', 'mmy0ca819v07kxej');
             }catch(PDOException $prueba_error){
                 echo "Error: " . $prueba_error->getMessage();
             }

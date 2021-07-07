c51bee533618fbe33368be512768cf98ea8c8d99
EDITH-R <rese-97@hotmail.com>
2021-07-07 11:51:23 -0500

    conexion p2

--- tlaxroom/Config/Config.php
+++ tlaxroom/Config/Config.php
@@ -1,9 +1,13 @@
 <?php
    const URL = "http://localhost/logintlaxroom/tlaxroom";
-    define ('DB_HOST', 's465z7sj4pwhp7fn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
-  define ('DB_NAME', 'dj9c9o63euq4l65z');
-  define ('DB_USER', 'qt435sf8tr9pjogv');
-  define ('DB_PASSWORWD', 'ww3hcmqmp8417adt');
-  define ('CHARSET', 'utf8');
+   //Zona horaria
+   date_default_timezone_set('America/Mexico_City');
+
+   //Datos de conexión a Base de Datos
+   define ('DB_HOST', 's465z7sj4pwhp7fn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+   define ('DB_NAME', 'dj9c9o63euq4l65z');
+   define ('DB_USER', 'qt435sf8tr9pjogv');
+   define ('DB_PASSWORD', 'ww3hcmqmp8417adt');
+   define ('DB_CHARSET', 'utf8');
 
 ?>

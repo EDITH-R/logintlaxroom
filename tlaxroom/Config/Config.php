734a19d03290c80dcf952962ec9774c54d75ddc2
EDITH-R <rese-97@hotmail.com>
2021-07-07 11:31:32 -0500

    conexion

--- tlaxroom/Config/Config.php
+++ tlaxroom/Config/Config.php
@@ -1,8 +1,9 @@
 <?php
-    const URL = "http://localhost/tlaxroom/";
-    const HOST = "localhost:3307";
-    const BD = "dbtlaxroomm";
-    const DB_USER = "root";
-    const PASS = "";
-    const CHARSET = "charset=utf8";
+   const URL = "http://localhost/logintlaxroom/tlaxroom";
+    define ('HOST', 's465z7sj4pwhp7fn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+  define ('DB', 'dj9c9o63euq4l65z');
+  define ('DB_USER', 'qt435sf8tr9pjogv');
+  define ('PASS', 'ww3hcmqmp8417adt');
+  define ('CHARSET', 'utf8');
+
 ?>

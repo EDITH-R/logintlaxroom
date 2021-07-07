75db329908933423ec15e0266e9f76c50845028a
EDITH-R <rese-97@hotmail.com>
2021-07-07 11:36:28 -0500

    servirdor

--- tlaxroom/Config/Config.php
+++ tlaxroom/Config/Config.php
@@ -2,7 +2,7 @@
    const URL = "http://localhost/logintlaxroom/tlaxroom";
     define ('HOST', 's465z7sj4pwhp7fn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
   define ('DB', 'dj9c9o63euq4l65z');
-  define ('DB_USER', 'qt435sf8tr9pjogv');
+  define ('USER', 'qt435sf8tr9pjogv');
   define ('PASS', 'ww3hcmqmp8417adt');
   define ('CHARSET', 'utf8');
 
